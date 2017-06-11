6729dcca600fb09f560251aa6283a70ca99b8c01
Marc <Marc@MarcPC.fritz.box>
2017-06-11 20:43:44 +0200

    Adding Kontenverwaltung and Konto

---
+++ src/Konto.java
@@ -0,0 +1,26 @@
+
+public class Konto {
+
+	private int ID_T;
+	private String titel;
+	private sollSeite Kontoseite;
+	private habenSeite kontoseite;
+	private String verrechnungKonto;
+
+	public Konto() {
+
+	}
+
+	private void saldieren() {
+
+	}
+
+	private void buchung(Buchungssatz bsatz) {
+
+	}
+
+	private void rueckbuchung(Buchungssatz bsatz) {
+
+	}
+
+}
