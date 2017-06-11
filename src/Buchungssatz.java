009f1c7f122d2e3fa72ea2ded72ab228becdf06f
Tanja <Tanja@DESKTOP-P7ML6AS.fritz.box>
2017-06-11 20:43:53 +0200

    Klaaaa

--- src/Buchungssatz.java
+++ src/Buchungssatz.java
@@ -1,4 +1,21 @@
 
 public class Buchungssatz {
 
+	private String ID_B;
+	private String name;
+	private String sollKonto;
+	private String habenKonto;
+	private double betrag;
+	private String versionBS;
+	
+	public Buchungssatz(long ID_G){
+		this.ID_B = ID_G + versionBS;
+		
+	}
+	
+	private String erhoeheVersion(String versionBS){
+		
+		return versionBS;
+		
+	}
 }
--- src/Geschaeftsfall.java
+++ src/Geschaeftsfall.java
@@ -1,4 +1,9 @@
 
 public class Geschaeftsfall {
 
+	private long ID_G;
+	private String titel;
+	private String beschreibung;
+	private ArrayList<Buchungssatz> saetze;
+	
 }
