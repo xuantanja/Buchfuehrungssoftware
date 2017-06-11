b24db20dc3f3030819d9dace6568f7f0fec79686
Tanja <Tanja@DESKTOP-P7ML6AS.fritz.box>
2017-06-11 14:47:52 +0200

    K

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,13 @@
 
+/* Regeln:
+ * 1. nicht in einer Methode arbeiten / bzw. nicht bevorzugt in einer klasse
+ * 2. Commit--> Pull--> Push
+ * Beim Hochladen: nur "commit" und NICHT "commit and push" -> Schreibst es in github "meine Änderungen",
+ * dann unbedingt pull (aktuellen stand runterladen), Dann push (hochladen für alle)
+ * 3. Oder Pull --> commit--> push
+ * 4. Kontakt miteinader haben/Absprache :D 
+ */
+
 public class GUI {
 
 	public static void main(String[] args) {
