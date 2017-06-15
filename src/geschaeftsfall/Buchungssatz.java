39663d975272ed09ff437c6c8adc28dd1193d0e5
Marc <Marc@MarcPC.fritz.box>
2017-06-15 11:43:49 +0200

    Getter und Setter vom Buchungssatz hinzugefügt Kontenseite Variablen und Methoden hinzugefügt

--- src/geschaeftsfall/Buchungssatz.java
+++ src/geschaeftsfall/Buchungssatz.java
@@ -17,4 +17,46 @@ public class Buchungssatz {
 		return versionBS;
 		
 	}
+
+	public String getID_B() {
+		return ID_B;
+	}
+
+	public void setID_B(String iD_B) {
+		ID_B = iD_B;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSollKonto() {
+		return sollKonto;
+	}
+
+	public void setSollKonto(String sollKonto) {
+		this.sollKonto = sollKonto;
+	}
+
+	public String getHabenKonto() {
+		return habenKonto;
+	}
+
+	public void setHabenKonto(String habenKonto) {
+		this.habenKonto = habenKonto;
+	}
+
+	public double getBetrag() {
+		return betrag;
+	}
+
+	public void setBetrag(double betrag) {
+		this.betrag = betrag;
+	}
+	
+	
 }
--- src/konten/Kontoseite.java
+++ src/konten/Kontoseite.java
@@ -1,7 +1,25 @@
 package konten;
 
+import java.util.HashMap;
+
+import geschaeftsfall.Buchungssatz;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class Kontoseite {
 	
 	private boolean sollseite;
+	private HBox container;
+	private VBox refName;
+	private VBox refBetrag;
+	private HashMap<String, Buchungssatz> buchungen;
+	
+	private void aktualisieren(){
+		
+	}
+	
+	private double getBetragssumme(){
+		return 0;
+	}
+	
 }
