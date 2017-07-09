cf1ba66006f759fa361cf5b299281d80d91acecf
Marc <Marc@MarcPC.fritz.box>
2017-07-09 15:42:21 +0200

    Bestandskonto anfangsbestand hinzugefügt

--- src/konten/Bestandskonto.java
+++ src/konten/Bestandskonto.java
@@ -2,7 +2,7 @@ package konten;
 
 public class Bestandskonto extends Konto{
 
-	public Bestandskonto(String titel, String kuerzel, String verrechnungskonto, int ID_T) {
+	public Bestandskonto(String titel, String kuerzel, String verrechnungskonto, int ID_T, double anfangsbestand) {
 		super(titel,kuerzel, verrechnungskonto, ID_T);
 	}
 
