73f5a5fbcca13acba992618b4cbac07257c699ba
Marc <Marc@MarcPC.fritz.box>
2017-07-27 11:33:31 +0200

    Import delete

--- src/application/menu/datei/BilanzErstellenController.java
+++ src/application/menu/datei/BilanzErstellenController.java
@@ -9,7 +9,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.ResourceBundle;
 
 import application.Kontenverwaltung;
