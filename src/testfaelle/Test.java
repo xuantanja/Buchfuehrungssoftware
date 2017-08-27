2860e6bb3e9c9c464eac7386c2012aec332ea1a1
tmanlik <tmanlik@BOCMOBIL11.boc-de.com>
2017-08-27 12:08:16 +0200

    EK nun Aktivkonto; JUnitTests

--- src/testfaelle/Test.java
+++ src/testfaelle/Test.java
@@ -1,16 +1,43 @@
 package testfaelle;
 
-import static org.junit.Assert.*;
-
+import application.GUI;
 import application.Kontenverwaltung;
+import geschaeftsfall.Buchungssatz;
+import geschaeftsfall.Geschaeftsfall;
+import javafx.application.Application;
+import konten.Bestandskonto;
 
 public class Test {
-
+	
+	//GUI startet in einem Thread
+	@org.junit.BeforeClass
+	public static void beforeClass() throws InterruptedException {
+		Thread t = new Thread("JavaFX Testing Thread") {
+			public void run() {
+				Application.launch(GUI.class, new String[]{});
+			}
+		};
+		t.setDaemon(true);
+		t.start();
+		Thread.sleep(1000);
+	}
+	
+	/*
+	 * Testfall, ob ein Buchungssatz einem Geschäftsfall hinzugefügt wird
+	 */
 	@org.junit.Test
-	public void test() {
+	public void test_BSHinzufuegen() {
 		Kontenverwaltung kv = new Kontenverwaltung();
-		//kv.addBuchungssatz(gfall, bsatz);
 		
+		Bestandskonto a = new Bestandskonto("y", "y", "x", 0, false);
+		Bestandskonto b = new Bestandskonto("x", "x", "y", 0, false);
+		
+		Geschaeftsfall g = new Geschaeftsfall(0, "gs1", "");
+		Buchungssatz bsatz = new Buchungssatz("", "a", "b", 7.0);
+		kv.addGeschaeftsfall(g);
+		kv.addBuchungssatz(g, bsatz);
+		
+		//Wieviele Bucungssätze sind im ersten Geschäftsfall (g = get(0))?
 		int anzahl = kv.getFaelle().get(0).getSaetze().size();
 		org.junit.Assert.assertEquals(1, anzahl);
 	}
