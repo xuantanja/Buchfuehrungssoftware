be8e705dce0e2c0d653065615581c45186350d40
Tanja <Tanja@DESKTOP-P7ML6AS>
2017-08-27 20:56:10 +0200

    Comp

--- src/utility/comparator/KontoComparator.java
+++ src/utility/comparator/KontoComparator.java
@@ -8,10 +8,6 @@ public class KontoComparator implements Comparator<Konto> {
 
 	@Override
 	public int compare(Konto o1, Konto o2) {
-		o1.getVerrechnungKonto();
-		o1.getKuerzel();
-		o2.getVerrechnungKonto();
-		o2.getKuerzel();
 		if (o2.getVerrechnungKonto() == o1.getKuerzel() || o1.getKontoart() == 4) {
 			return 1;
 		}
